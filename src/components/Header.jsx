a5fd6c33fa972e9ff84f711e137ef6514022bca5
lucifer098a <nk5141030@gmail.com>
2022-08-12 02:19:35 +0530

    recommend start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
---
+++ src/components/Recommends.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Recommends = () => {
+  return (
+    <div>
+      <h4>Recommended for you</h4>
+    </div>
+  );
+};
+
+export default Recommends;
