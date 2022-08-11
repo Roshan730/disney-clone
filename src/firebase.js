a7870d4c9311b38375f0dd56f59425dfb36c8744
lucifer098a <nk5141030@gmail.com>
2022-08-12 01:26:00 +0530

    slider

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   storageBucket: "disney-clone-a5657.appspot.com",
   messagingSenderId: "318996085088",
   appId: "1:318996085088:web:ea1ce270f379a6a8d24605",
-  measurementId: "G-E5X2DF1MYY",
+  measurementId: "G-E5X2DF1MYY"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
